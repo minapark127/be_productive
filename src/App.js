d138777d1b131c773f7da6e0adba505e942ff901
Mina <mina.park127@gmail.com>
2020-08-20 22:53:25 +1000

    adding to dos

--- src/App.js
+++ src/App.js
@@ -1,7 +1,56 @@
-import React from "react";
+import React, { useReducer, useState } from "react";
+
+const initialState = {
+  toDos: [],
+};
+
+const ADD = "add";
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case ADD:
+      return { toDos: [...state.toDos, { text: action.payload }] };
+    default:
+      return;
+  }
+};
 
 function App() {
-  return <div className="App"></div>;
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const [newToDo, setNewToDo] = useState("");
+  const onSubmit = (e) => {
+    e.preventDefault();
+    dispatch({ type: ADD, payload: newToDo });
+    setNewToDo("");
+  };
+  const onChange = (e) => {
+    const {
+      target: { value },
+    } = e;
+    setNewToDo(value);
+  };
+  return (
+    <>
+      <h1>be productive</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          type="text"
+          placeholder="add a task"
+          onChange={onChange}
+          value={newToDo}
+        />
+        <input type="button" value="add" onClick={onSubmit} />
+      </form>
+      {state.toDos.length > 0 ? (
+        <ul>
+          <h2>To Dos</h2>
+          {state.toDos.map((toDo, index) => (
+            <li key={index}>{toDo.text}</li>
+          ))}
+        </ul>
+      ) : null}
+    </>
+  );
 }
 
 export default App;
