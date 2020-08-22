e5d963d3478b29f2e6b9ee5838530dbfe42dd4cd
Mina <mina.park127@gmail.com>
2020-08-23 00:09:50 +1000

    Updates

---
+++ precache-manifest.8db93f2e942952fef83a039ef114a686.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1ae4c446c3827c9c08f2d79c1781a7f",
+    "url": "/minapark127/be_productive/index.html"
+  },
+  {
+    "revision": "634b9e1501598cdb96b9",
+    "url": "/minapark127/be_productive/static/js/2.4a5b6725.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/minapark127/be_productive/static/js/2.4a5b6725.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7381c4f7a031bd7f527",
+    "url": "/minapark127/be_productive/static/js/main.8b6c0ccb.chunk.js"
+  },
+  {
+    "revision": "8c96b2a58056ff430398",
+    "url": "/minapark127/be_productive/static/js/runtime-main.2eec6409.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/minapark127/be_productive/precache-manifest.8db93f2e942952fef83a039ef114a686.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/minapark127/be_productive/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
